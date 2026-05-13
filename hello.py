96340ef2f100757ef94d039cd35d45c22f680734
gustavoesquivel-design <tavoesquivel@gmail.com>
2026-05-13 18:12:07 -0500

    CS50 Homework and exercises

--- hello.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print ("Hello, World!")
-    print ("Welcome to my first python program.")
-    print ("This was just to show myself that I could do it.")
-    print ("That's all for today.")
-
-main()
